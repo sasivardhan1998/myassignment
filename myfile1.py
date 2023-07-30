b5ff3995d2e414d19e3a7332116010edb10a487e
sasivardhan1998 <sasivardham.marolix@gmail.com>
2023-07-30 16:31:30 +0530

    initial commit

---
+++ myfile1.py
@@ -0,0 +1,8 @@
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+if (num1>num2):
+    print(num1," is a greater")
+elif(num2>num1):
+    print(num2, "is a smaller")
+else:
+    ("both are equal")
