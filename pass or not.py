1439984b4e070aa46c52bd2fb7132e5c8c9b5cb0
sasivardhan1998 <sasivardham.marolix@gmail.com>
2023-07-30 18:09:46 +0530

    initial commit

---
+++ pass or not.py
@@ -0,0 +1,32 @@
+#print('enter marks obtained in 3 subjects')
+#s1=int(input())
+#s2=int(input())
+#s3=int(input())
+#avg=s1+s2+s3/3
+#if(s1>40 and s2>40):
+#    print("pass")
+#elif (s2>40 and s3>30):
+#    print(" pass")
+#elif (s3>40 and s1>40):
+#    print("pass")
+#elif avg>40:
+#    print("pass")
+#else:
+ #  print("fail")
+print('enter marks obtaines in 3 subject')
+s1=int(input())
+s2=int(input())
+s3=int(input())
+avg=s1+s2+s3/3
+if(s1>40 and s2>40):
+    print("pass")
+elif(s2>40 and s3>40):
+    print("pass")
+elif(s3>40 and s1>40):
+    print("pass")
+elif (avg>40):
+    print("pass")
+else:
+    print("fail")
+
+
